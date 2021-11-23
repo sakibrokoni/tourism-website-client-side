4baec34ca9da5049f9a52144d292d16f838aa32c
Sakib Rokoni <66318575+sakibrokoni@users.noreply.github.com>
2021-11-24 02:24:00 +0600

    Update firebase.config.js

--- src/components/Login/Firebase/firebase.config.js
+++ src/components/Login/Firebase/firebase.config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBB29tkpHSEUW3sSTJ-ndvIV1zWzUozJKA",
-    authDomain: "simple-firebase-4a4da.firebaseapp.com",
-    projectId: "simple-firebase-4a4da",
-    storageBucket: "simple-firebase-4a4da.appspot.com",
-    messagingSenderId: "841656631409",
-    appId: "1:841656631409:web:208b16ff2db27bbb701bd0"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 export default firebaseConfig;  
